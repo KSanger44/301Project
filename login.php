8eec5e99d6d2f5783ac3fab59ada7bc3fc33c493
KSanger44 <112338808+KSanger44@users.noreply.github.com>
2022-11-19 17:36:11 -0800

    login working

---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<?php
+   include("config.php");
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $mylogin = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT * FROM patient WHERE email = '$mylogin' and pw = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      //$active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         //session_register("myusername");
+         $_SESSION['login_user'] = $mylogin;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+   }
+?>
+<html>
+<head>
+    <title>Bootstrap Assignment</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<div class="container login-container">
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Login</h3>
+            <form action="" method="post">
+                <div class="form-group">
+                    <input type="text" class="form-control" name = "username" placeholder="Username" value="" />
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" name = "password" placeholder="Password" value="" />
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btnSubmit" value="Login" />
+                </div>
+                <div class="form-group">
+                    <a href="#" class="ForgetPwd">Forgot Password?</a>
+                    <span id="forgotpwdsent"></span>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
