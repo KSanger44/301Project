2ca9ef32f2d637efa93044c80ed84a9cc3e841a5
Kyle Sanger <ksanger@edgewood.edu>
2022-11-25 13:20:33 -0600

    sessions

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
+session_start();
 <!DOCTYPE html>
 <?php
    include("config.php");
-   session_start();
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
---
+++ patientStatus.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <h4>Hello ksanger,</h4>
+
+    <div class="container-fluid">
+        <div class="btn-group-vertical opacity-25" role="group" aria-label="Basic example">
+            <button type="button" id="prep" class="btn btn-danger">Preparing for Surgery</button>
+            <button type="button" id="surgery" class="btn btn-warning">In Surgery</button>
+            <button type="button" id="recovery" class="btn btn-primary">Recovering</button>
+            <button type="button" id="checkout" class="btn btn-success">Ready to checkout</button>
+          </div>
+    </div>
+    <div id="nextAction">
+        <p>Kyle is scheduled for a consultation with Dr. Susan I. Toth, MD at 8am on 1/1/2023</p>
+    </div>
+    <div class="btn-group">
+        <a href="checkin.html" role="button" class="btn btn-danger">Check-in</a>
+        <a href="patientStatus.html" role="button" class="btn btn-primary">Patient Status</a>
+        <a href="procedureInfo.html" role="button" class="btn btn-primary">Procedure Info</a>
+        <a href="contacts.html" role="button" class="btn btn-primary">Contacts</a>
+    </div>
+</body>
+
+</html>
