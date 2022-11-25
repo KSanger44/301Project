d191e8acf594312e0de9bd5ef3d411a42622cb8f
Kyle Sanger <ksanger@edgewood.edu>
2022-11-25 12:25:25 -0600

    oop conn

--- config.php
+++ config.php
@@ -1,7 +1,17 @@
 <?php
-   define('DB_SERVER', 'localhost:3306');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'surgtrack');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+      //connect to the db schema
+      $servername = "localhost:3306";
+      $username = "root";
+      $password = '';
+      $dbname = "surg";
+
+      // Create connection
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+         // Check connection
+      if ($conn->connect_error) {
+         die("Connection failed: " . $conn->connect_error);
+   }
+   
 ?>
