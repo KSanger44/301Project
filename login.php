381a565b66e40eb4397af769cbd2b07e58f84af4
Kyle Sanger <ksanger@edgewood.edu>
2022-11-27 16:27:52 -0600

    header

--- login.php
+++ login.php
@@ -40,12 +40,14 @@
 ?>
 <html>
 <head>
+    <title>Surgery Tracker</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+    <h4>Surgery Tracker</h4><br>
 <div class="container login-container">
     <div class="row">
         <div class="col-md-6">
